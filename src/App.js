2edfb11d7dbb2b04552841904bd8fd0c2938744b
Magdalena Zieńko <Magdalena Zieko>
2017-04-22 13:10:16 +0200

    Fixing mispelling

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to Reaasdsact</h2>
+          <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get starasdsted, edit <code>src/App.js</code> and save to reload.
+          To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
